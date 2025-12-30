364df2cee3c41030462ba3acdf90e6e772fe20e0
Viwu <sinconsistencia@gmail.com>
2025-12-30 12:32:42 -0600

    level select console

--- dev/overscope.md
+++ dev/overscope.md
@@ -2,6 +2,7 @@
 - Git commit, branch, and local changes? displayed in main menu
 - Enum Abstract for platforms
 - Define Group Class that makes finding defines easier(?)
+- Enum Abstract for path extensions
 
 # Cant / Wont do
 - Github actions to automake a release every day
--- source/frontend/menus/LevelSelectState.hx
+++ source/frontend/menus/LevelSelectState.hx
@@ -1,5 +1,29 @@
 package frontend.menus;
 
+import flixel.FlxG;
+import backend.overwrites.Sprite;
 import backend.overwrites.State;
 
-class LevelSelectState extends State {}
+class LevelSelectState extends State
+{
+	public var console_center:Sprite = new Sprite('levelselect/console_center'.getImageFile());
+	public var console_side_left:Sprite = new Sprite('levelselect/console_side'.getImageFile());
+	public var console_side_right:Sprite = new Sprite('levelselect/console_side'.getImageFile());
+
+	override function create()
+	{
+		super.create();
+
+		for (console in [console_center, console_side_left, console_side_right])
+		{
+            console.screenCenter();
+            console.y = FlxG.height - console.height;
+
+			add(console);
+		}
+
+        console_side_left.x = 0;
+        console_side_right.flipX = true;
+        console_side_right.x = FlxG.width - console_side_right.width;
+	}
+}
