94dca0d4f2e7743027a1881b9b14358c1523df19
Viwu <sinconsistencia@gmail.com>
2025-12-29 23:21:12 -0600

    log version and defines

--- source/InitState.hx
+++ source/InitState.hx
@@ -1,5 +1,6 @@
 package;
 
+import backend.Global;
 import backend.overwrites.State;
 import frontend.menus.MainMenuState;
 
@@ -8,6 +9,9 @@ class InitState extends State
 	override public function create()
 	{
 		super.create();
+
+		trace('Version: ' + Global.VERSION);
+		trace('Defines: ' + Global.DEFINES);
 
 		switchState(new MainMenuState());
 	}
