e9737cb9c65867a834fa92626db9a07a191c7bc9
Viwu <sinconsistencia@gmail.com>
2025-12-29 23:11:55 -0600

    state watermarks

--- source/backend/Global.hx
+++ source/backend/Global.hx
@@ -10,4 +10,37 @@ class Global
 	{
 		return Assets.getText('version.txt');
 	}
+
+    public static var DEFINES(get, never):Array<String>;
+    
+    static function get_DEFINES():Array<String> {
+        var d = [];
+
+        // platforms
+        #if windows d.push('windows'); #end
+        #if macos d.push('macos'); #end
+        #if linux d.push('linux'); #end
+        #if html5 d.push('html5'); #end
+        #if hl d.push('hl'); #end
+        #if android d.push('android'); #end
+        #if ios d.push('ios'); #end
+
+        #if debug d.push('debug'); #end
+
+        return d;
+    }
+
+	public static var BUILD_DEBUG(get, never):Bool;
+
+	static function get_BUILD_DEBUG():Bool
+	{
+		return DEFINES.contains('debug');
+	}
+
+	public static var BUILD_PLATFORM(get, never):String;
+
+	static function get_BUILD_PLATFORM():String
+	{
+		return DEFINES[0];
+	}
 }
--- source/backend/overwrites/State.hx
+++ source/backend/overwrites/State.hx
@@ -1,23 +1,33 @@
 package backend.overwrites;
 
+import flixel.text.FlxText;
 import flixel.FlxG;
 import flixel.FlxState;
 
 class State extends FlxState
 {
-	override public function new()
-	{
-		super();
-	}
+	public var watermark_left:FlxText;
+	public var watermark_right:FlxText;
 
 	override function create()
 	{
 		super.create();
+
+		watermark_left = makeBaseText(Global.VERSION, LEFT, FlxG.width / 2);
+		watermark_right = makeBaseText('', RIGHT, FlxG.width / 2);
+		add(watermark_left);
+		add(watermark_right);
+
+		if (Global.BUILD_DEBUG)
+			watermark_left.text += ' (DEBUG)';
 	}
 
-	override function update(elapsed:Float)
+	public function makeBaseText(text:String, alignment:FlxTextAlign = LEFT, ?fieldWidth = 0.0):FlxText
 	{
-		super.update(elapsed);
+		var new_text:FlxText = new FlxText(0, 0, fieldWidth, text, 16);
+		new_text.alignment = alignment;
+
+		return new_text;
 	}
 
 	public function switchState(state:FlxState)
