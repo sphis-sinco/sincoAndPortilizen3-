f1aca7fb1178dfc639047faf3dd04dc46a7a1892
Viwu <sinconsistencia@gmail.com>
2025-12-29 23:24:04 -0600

    sprite overwrite class

---
+++ source/backend/overwrites/Sprite.hx
@@ -0,0 +1,25 @@
+package backend.overwrites;
+
+import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.FlxSprite;
+
+class Sprite extends FlxSprite
+{
+	public static var SCALE:Float = 2.0;
+
+    public var scaleOffset:Float = 0.0;
+
+	override public function new(simpleGraphic:FlxGraphicAsset, ?scaleOffset:Float = 0, ?x:Float, ?y:Float)
+	{
+		super(x, y, simpleGraphic);
+
+		resetScale();
+        this.scaleOffset = scaleOffset;
+	}
+
+	public function resetScale()
+	{
+		scale.set(SCALE - scaleOffset, SCALE - scaleOffset);
+		updateHitbox();
+	}
+}
